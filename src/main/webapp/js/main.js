7c839eb1115573260f462b75b276e5b449a70a45
DESKTOP-P2VQDL1\csi22 <ballesterospereze@gmail.com>
2024-12-05 11:25:52 +0100

    Import images method

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -1,3 +1,10 @@
-import { gestionFormulario } from "./function.js";
 
-gestionFormulario();
+import { manejarFormulario, verEnGrande, cerrarImagen } from './function.js';
+
+
+// Asociar el evento de envío del formulario con la función manejarFormulario
+document.getElementById("formularioUsuario").addEventListener("submit", manejarFormulario);
+
+// Asociar las funciones de visualización de imagen grande
+window.verEnGrande = verEnGrande;
+window.cerrarImagen = cerrarImagen;
